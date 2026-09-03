436cdf19249418b93ce294725153ea1b3f4025fc
nekorei05 <reikhandekar05@gmail.com>
2026-09-03 19:00:47 +0530

    feat: implemented authentication

---
+++ src/main/java/com/example/flight/service/AuthService.java
@@ -0,0 +1,53 @@
+package com.example.flight.service;
+
+import com.example.flight.entity.User;
+import com.example.flight.enums.UserRole;
+import com.example.flight.model.LoginRequest;
+import com.example.flight.model.RegisterRequest;
+import com.example.flight.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public AuthService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder,  JwtService jwtService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+    }
+
+    public User register(RegisterRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already registered");
+        }
+
+        User user = new User();
+
+        user.setFullName(request.getFullName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setMobileNumber(request.getMobileNumber());
+        user.setRole(UserRole.PASSENGER);
+
+        return userRepository.save(user);
+    }
+
+    public String login(LoginRequest request) {
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("Invalid email or password"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid email or password");
+        }
+
+        return jwtService.generateToken(user);
+    }
+}
---
+++ src/main/java/com/example/flight/service/JwtService.java
@@ -0,0 +1,53 @@
+package com.example.flight.service;
+
+import com.example.flight.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretString;
+
+    private final long jwtExpirationMs = 86400000; // 24 hours
+
+    private SecretKey getSignInKey() {
+        return Keys.hmacShaKeyFor(
+                secretString.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+}
