28cc8f5a06bb447b0d14e92bde40c33afe31a214
nekorei05 <reikhandekar05@gmail.com>
2026-09-09 11:58:36 +0530

    chore : updated api paths

--- src/main/java/com/example/flight/controller/AirlineController.java
+++ src/main/java/com/example/flight/controller/AirlineController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/airlines")
+@RequestMapping("/api/airlines")
 @CrossOrigin(origins = "http://localhost:4200")
 public class AirlineController {
 
--- src/main/java/com/example/flight/controller/AuthController.java
+++ src/main/java/com/example/flight/controller/AuthController.java
@@ -11,7 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
+
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
